619734890acfc85faefc4d976882090ba960ee30
👨🏾‍💻 Henry AT <yiperu@gmail.com>
2020-06-16 23:09:41 -0400

    Create a struct for this example In this case create a class cart, that have a static method

--- app/src/main/java/com/example/lazy_evaluation_delegadoy_by/Cart.kt
+++ app/src/main/java/com/example/lazy_evaluation_delegadoy_by/Cart.kt
@@ -1,2 +1,23 @@
 package com.example.lazy_evaluation_delegadoy_by
 
+class Cart {
+    val paymentConfig = Cart.loadAndPreparePaymentConfig()
+
+    fun checkout() {
+        if (paymentConfig["type"] == "card") {
+            // Do something
+        }
+    }
+
+    companion object {
+        fun loadAndPreparePaymentConfig(): Map<String, String> {
+            println("Very Expansive Operation!!!!!.")
+            return mapOf("currency" to "eur", "type" to "card")
+        }
+    }
+
+}
+
+fun main(args: Array<String>) {
+
+}
